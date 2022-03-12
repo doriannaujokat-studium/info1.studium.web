4fa82cffda98ce9b188e2bbe2764003fe9d3d2ce
Dorian Naujokat <35139623+doriannaujokat@users.noreply.github.com>
2022-03-12 21:32:28 +0100

    Updated Turing-Simulator

--- turing/simulator.js
+++ turing/simulator.js
@@ -1,5 +1,19 @@
 (() => {
-    window.addEventListener('load', () => {
+    const LANG = {
+        EMPTCHAR_NOT_BANDCHAR: "Das Leerzeichen ist nicht in der Bandmenge definiert.",
+        INCHAR_NOT_BANDCHAR: "Alle Eingabecharaktere müssen in der Bandmenge definiert sein.",
+        STARTCHAR_NOT_INCHAR: "Der Anfangswert des Bandes kann nur aus Eingabecharakteren bestehen!",
+        STARTSTATE_NOT_DEFINED: "Ein Anfangszustand muss definiert sein!",
+        NEXTSTEP_NA: "Es kann kein weiterer Schritt gemacht werden!",
+        ENDSTATE_ARRIVED: "Das Ende wurde erreicht!",
+        TRANCHAR_NOT_BANDCHAR: "Alle übergangswerte müssen in der Bandmenge definiert sein.",
+        NOT_READY: "Die Maschine ist nicht bereit.",
+    };
+
+    window.addEventListener('load', async () => {
+        const TMModule = await import('./turing.js');
+        const TuringMachine = TMModule.default;
+
         let DOM_Z_empty = document.getElementById('turing-list-Z-empty');
         let DOM_Z_list = document.getElementById('turing-list-Z'); // Zustände
         let DOM_Band_list = document.getElementById('turing-list-Band');
@@ -15,12 +29,9 @@
          */
         let ctx = canvas.getContext('2d');
 
-        const Transitions = new Map();
+        /** @type {TuringMachine} */
+        const Machine = new TuringMachine();
 
-        const Band = new Map();
-        let BandPos = -1;
-        let CurrentState = 'z1';
-        let emptyChar = '_';
         let simulating = false;
         let simulatingTimer = undefined;
 
@@ -100,46 +111,27 @@
         }
         [DOM_Band_list,DOM_E_list,DOM_B_list,DOM_StartZ,DOM_emptyZ,DOM_T_list].forEach(d => d.addEventListener('input',saveToLocal));
         function resetTuring() {
-            Band.clear();
-            BandPos = -1;
-            Transitions.clear();
+            Machine.reset();
             output.value = "";
             DOM_Z_list.innerHTML = "";
             DOM_Z_empty.style.display = "block";
+            document.getElementById('turing-play').disabled = true;
+            document.getElementById('turing-step').disabled = true;
             draw();
-            {
-                let BandChars = DOM_B_list.value!==""?DOM_B_list.value.split(""):[];
-                emptyChar = DOM_emptyZ.value!==""?DOM_emptyZ.value:'_';
-                if (!BandChars.includes(emptyChar)) {
-                    alert("Das Leerzeichen ist nicht in der Bandmenge definiert.");
-                    return;
-                }
-                let InputChars = DOM_E_list.value!==""?DOM_E_list.value.split(""):[];
-                if (!InputChars.every(z => BandChars.includes(z))) {
-                    alert("Alle Eingabecharaktere müssen in der Bandmenge definiert sein.");
-                    return;
-                }
-                let BandValue = DOM_Band_list.value!==""?DOM_Band_list.value.split(""):[];
-                if (!BandValue.every(z => InputChars.includes(z)||emptyChar === z)) {
-                    alert("Der Anfangswert des Bandes kann nur aus Eingabecharakteren bestehen!");
-                    return;
-                }
-                BandValue.forEach((z,i) => {
-                    Band.set(i,z);
-                });
-                output.value = Array.from(Band.entries()).sort(([k1,v1],[k2,v2]) => k1 -k2).map(v => v[1]).join("");
-                if (DOM_StartZ.value === "") {
-                    alert("Ein Anfangszustand muss definiert sein!");
-                    return;
-                }
-                CurrentState = DOM_StartZ.value;
-            }
+
+            let BandChars = DOM_B_list.value!==""?DOM_B_list.value.split(""):[];
+            let EmptyChar = DOM_emptyZ.value!==""?DOM_emptyZ.value:'_';
+            let InputChars = DOM_E_list.value!==""?DOM_E_list.value.split(""):[];
+            let BandValue = DOM_Band_list.value!==""?DOM_Band_list.value:"";
+            let Transitions = [];
             let Tval = DOM_T_list.value;
             if (Tval !== "") {
                 let items = Tval.split("\n");
-                items.forEach(v => {
-                    let imatch = v.match(new RegExp(`\\((\\w+),(.)\\)\\s*\\((\\w+),(.),([RLH])\\)`,'i'));
-                    if (!imatch) return;
+                for (let v of items) {
+                    //let imatch = v.match(new RegExp(`\\((\\w+),(.)\\)\\s*\\((\\w+),(.),([RLH])\\)`,'i'));
+                    let imatch = v.match(new RegExp(`^\\(?(?<oState>\\w+),\\s*?(?<oChar>.)\\s*?(?:(?<!\\(.*?),\\s*?|(?<=\\(.*?)\\)\\s*?(?:[-=]*>)?\\s*?\\()(?<nState>\\w+),\\s*?(?<nChar>.),\\s*?(?<action>R|L|H)(?:(?<=\\(.*?\\).*?\\(.*?)\\)$|(?<!\\(.*?)$)`,'iu'));
+                    if (!imatch) continue;
+                    /**
                     if (!Array.from(DOM_Z_list.childNodes.values()).some(v => v.innerText === imatch[1])) {
                         let d = document.createElement('div');
                         d.innerText = imatch[1];
@@ -150,71 +142,125 @@
                         d.innerText = imatch[3];
                         DOM_Z_list.appendChild(d);
                     }
-                    DOM_Z_empty.style.display = "none";
-                    let ts = Transitions.get(imatch[1]);
-                    if (!ts) {
-                        ts = new Map();
-                        Transitions.set(imatch[1],ts);
-                    }
-                    ts.set(imatch[2],[imatch[3],imatch[4],imatch[5]]);
-                });
+                    DOM_Z_empty.style.display = "none";*/
+                    Transitions.push({oState: imatch.groups.oState, oChar: imatch.groups.oChar, nState: imatch.groups.nState, nChar: imatch.groups.nChar, action: imatch.groups.action});
+                }
             }
+            let loadParam = {BandChars: BandChars, InputChars: InputChars, BandValue: BandValue, EmptyChar: EmptyChar, StartState: DOM_StartZ.value, Transitions: Transitions};
+            let validation = Machine.validate(loadParam);
+            if (validation !== "VALID") {
+                let message;
+                switch (validation) {
+                    case "INVALID_INC":
+                        message = LANG.INCHAR_NOT_BANDCHAR;
+                        break;
+                    case "INVALID_EMPTC":
+                        message = LANG.EMPTCHAR_NOT_BANDCHAR;
+                        break;
+                    case "INVALID_TRANS":
+                        message = LANG.TRANCHAR_NOT_BANDCHAR;
+                        break;
+                    default:
+                        message = validation;
+                        break;
+                }
+                alert(message);
+                return;
+            }
+            Machine.load(loadParam);
+            output.value = Machine.getBandValues();
+            document.getElementById('turing-play').disabled = false;
+            document.getElementById('turing-step').disabled = false;
             draw();
+
         }
-        function performStep() {
-            if (!Transitions.has(CurrentState) || !Transitions.get(CurrentState).has(getCurrentValue())) {
+        function timedStep() {
+            const result = Machine.performStep();
+            if (result === "NOT_READY") {
                 if (simulating) {
                     document.getElementById('turing-play').click();
                 }
-                alert("Es kann kein weiterer Schritt gemacht werden!");
+                alert(LANG.NOT_READY);
                 return;
             }
-            let ct = Transitions.get(CurrentState).get(getCurrentValue());
-            CurrentState = ct[0];
-            Band.set(BandPos,ct[1]);
-            switch(ct[2]) {
-                case 'L':
-                    BandPos--;
-                    break;
-                case 'R':
-                    BandPos++;
-                    break;
-                case 'H':
-                    if (simulating) {
-                        document.getElementById('turing-play').click();
-                        break;
-                    }
-                    alert("Das Ende wurde erreicht!");
-                    break;
+            if (result === "NO_TRANSITION") {
+                if (simulating) {
+                    document.getElementById('turing-play').click();
+                }
+                alert(LANG.NEXTSTEP_NA);
+                return;
             }
-            output.value = Array.from(Band.entries()).sort(([k1,v1],[k2,v2]) => k1 -k2).map(v => v[1]).join("");
+            output.value = Machine.getBandValues();
             draw();
+            if (result === "HALT") {
+                if (simulating) {
+                    document.getElementById('turing-play').click();
+                }
+                alert(LANG.ENDSTATE_ARRIVED);
+                return;
+            }
         }
 
         document.getElementById('turing-reset').addEventListener('click', resetTuring);
+        document.getElementById('turing-fast').addEventListener('click', () => {
+            if (simulating) {
+                clearInterval(simulatingTimer);
+                if (document.getElementById('turing-fast').hasAttribute('active')) {
+                    simulatingTimer = setInterval(timedStep, 250);
+                    document.getElementById('turing-fast').removeAttribute('active');
+                    return;
+                }
+                document.getElementById('turing-fast50').removeAttribute('active');
+                simulatingTimer = setInterval(timedStep, 25);
+                document.getElementById('turing-fast').setAttribute('active','');
+            }
+        });
+        document.getElementById('turing-fast50').addEventListener('click', () => {
+            if (simulating) {
+                clearInterval(simulatingTimer);
+                if (document.getElementById('turing-fast50').hasAttribute('active')) {
+                    simulatingTimer = setInterval(timedStep, 250);
+                    document.getElementById('turing-fast50').removeAttribute('active');
+                    return;
+                }
+                document.getElementById('turing-fast').removeAttribute('active');
+                simulatingTimer = setInterval(timedStep, 5);
+                document.getElementById('turing-fast50').setAttribute('active','');
+            }
+        });
         document.getElementById('turing-play').addEventListener('click', () => {
             if (simulating) {
                 clearInterval(simulatingTimer);
-                document.getElementById('turing-reset').disabled = false;
                 document.getElementById('turing-clear').disabled = false;
+                document.getElementById('turing-example').disabled = false;
+                document.getElementById('turing-load').disabled = false;
+                document.getElementById('turing-reset').disabled = false;
                 document.getElementById('turing-step').disabled = false;
-                document.getElementById('turing-play').innerText = "Start";
+                document.getElementById('turing-fast').disabled = true;
+                document.getElementById('turing-fast50').disabled = true;
+                document.getElementById('turing-play').removeAttribute('active');
+                document.getElementById('turing-fast').removeAttribute('active');
+                document.getElementById('turing-fast50').removeAttribute('active');
+                document.getElementById('turing-play-text').innerText = "Start";
+                document.getElementById('turing-play-icon').setAttribute('d',"M6 3 A 1 1 0 0 0 5 4 A 1 1 0 0 0 5 4.0039062L5 15L5 25.996094 A 1 1 0 0 0 5 26 A 1 1 0 0 0 6 27 A 1 1 0 0 0 6.5800781 26.8125L6.5820312 26.814453L26.416016 15.908203 A 1 1 0 0 0 27 15 A 1 1 0 0 0 26.388672 14.078125L6.5820312 3.1855469L6.5800781 3.1855469 A 1 1 0 0 0 6 3 z");
                 simulating = false;
                 simulatingTimer = undefined;
                 return;
             }
             simulating = true;
-            document.getElementById('turing-reset').disabled = true;
             document.getElementById('turing-clear').disabled = true;
+            document.getElementById('turing-example').disabled = true;
+            document.getElementById('turing-load').disabled = true;
+            document.getElementById('turing-reset').disabled = true;
             document.getElementById('turing-step').disabled = true;
-            document.getElementById('turing-play').innerText = "Stop";
-            simulatingTimer = setInterval(performStep, 250);
+            document.getElementById('turing-fast').disabled = false;
+            document.getElementById('turing-fast50').disabled = false;
+            document.getElementById('turing-play').setAttribute('active','');
+            document.getElementById('turing-play-text').innerText = "Stop";
+            document.getElementById('turing-play-icon').setAttribute('d',"M8 4C6.895 4 6 4.895 6 6L6 24C6 25.105 6.895 26 8 26L10 26C11.105 26 12 25.105 12 24L12 6C12 4.895 11.105 4 10 4L8 4 z M 20 4C18.895 4 18 4.895 18 6L18 24C18 25.105 18.895 26 20 26L22 26C23.105 26 24 25.105 24 24L24 6C24 4.895 23.105 4 22 4L20 4 z");
+            simulatingTimer = setInterval(timedStep, 250);
         });
-        document.getElementById('turing-step').addEventListener('click', performStep);
-
-        function getCurrentValue() {
-            return Band.get(BandPos)??emptyChar;
-        }
+        document.getElementById('turing-step').addEventListener('click', timedStep);
 
         window.addEventListener('resize', resize);
 
@@ -230,10 +276,11 @@
             const middleElem = [middle[0]-rectSize/2,middle[1]-rectSize/2];
 
             ctx.clearRect(0,0,width,height);
+            if (!Machine.isReady()) return;
 
-            for (let i = BandPos - Math.round(middleElem[0]/50); middleElem[0] - (BandPos-i)*(rectSize+margin) < width + rectSize + margin; i++) {
-                let value = Band.get(i)??emptyChar;
-                drawRect(i,value,middleElem[0] - (BandPos-i)*(rectSize+margin),middleElem[1],rectSize,rectSize);
+            for (let i = Machine.getCurrentPosition() - Math.round(middleElem[0]/50); middleElem[0] - (Machine.getCurrentPosition()-i)*(rectSize+margin) < width + rectSize + margin; i++) {
+                let value = Machine.getCharacterAtPosition(i);
+                drawRect(i,value,middleElem[0] - (Machine.getCurrentPosition()-i)*(rectSize+margin),middleElem[1],rectSize,rectSize);
             }
 
             ctx.strokeStyle = '#ff0000';
@@ -244,7 +291,7 @@
             ctx.font = "16px sans-serif";
             ctx.fillStyle = '#ff0000';
             //ctx.fillText(v,x+(w/2-measure.width/2),y+(h/2));
-            ctx.fillText(CurrentState,middleElem[0]+(rectSize/2),middleElem[1]-1);
+            ctx.fillText(Machine.getCurrentState(),middleElem[0]+(rectSize/2),middleElem[1]-1);
         }
         function drawRect(i,v,x,y,w,h) {
             ctx.fillStyle = Math.abs(i)%2===1?'#000000':'#89ba17';
---
+++ turing/turing.js
@@ -0,0 +1,154 @@
+/**
+ * @return {TuringMachine}
+ * @class TuringMachine
+ * @constructor
+ */
+function TuringMachine() {
+    if (!new.target) return new TuringMachine(...arguments);
+    this.reset();
+}
+
+/**
+ * @this {TuringMachine}
+ * @return {TuringMachine}
+ */
+function reset() {
+    /**
+     * @name TuringMachine#Band
+     * @type {?Map<number,string>}
+     * @private
+     */
+    this.Band = undefined;
+    /**
+     * @name TuringMachine#Position
+     * @type {?number}
+     * @private
+     */
+    this.Position = undefined;
+    /**
+     * @name TuringMachine#State
+     * @type {?string}
+     * @private
+     */
+    this.State = undefined;
+    /**
+     * @name TuringMachine#Transitions
+     * @type {?Map<string,Map<string,{char: string, state: string, action: "R"|"L"|"H"}>>}
+     * @private
+     */
+    this.Transitions = undefined;
+    /**
+     * @name TuringMachine#EmptyChar
+     * @type {?string}
+     * @private
+     */
+    this.EmptyChar = undefined;
+    return this;
+}
+TuringMachine.prototype.reset = reset;
+
+/**
+ * @param {{BandChars: string[], InputChars: string[], BandValue: string, EmptyChar: string, StartState: string, Transitions: {oState: string, oChar: string, nState: string, nChar: string, action: "R"|"L"|"H"}[]}} args
+ * @this {TuringMachine}
+ * @return {?TuringMachine}
+ */
+function load(args) {
+    if (validate(args) !== "VALID") return undefined;
+    this.Band = new Map();
+    this.Position = 0;
+    this.State = args.StartState;
+    this.Transitions = new Map();
+    this.EmptyChar = args.EmptyChar;
+    for (let t of args.Transitions) {
+        if (!this.Transitions.has(t.oState)) this.Transitions.set(t.oState, new Map());
+        let tm = this.Transitions.get(t.oState);
+        tm.set(t.oChar, {char: t.nChar, state: t.nState, action: t.action});
+    }
+    args.BandValue.split('').forEach((v,i) => v.length === 1 && this.Band.set(i,v));
+    return this;
+}
+TuringMachine.prototype.load = load;
+
+/**
+ * @param {{BandChars: string[], InputChars: string[], BandValue: string, EmptyChar: string, StartState: string, Transitions: {oState: string, oChar: string, nState: string, nChar: string, action: "R"|"L"|"H"}[]}} args
+ * @return {"VALID"|"INVALID_BANDC"|"INVALID_INC"|"INVALID_BANDV"|"INVALID_EMPTC"|"INVALID_TRANS"}
+ */
+function validate(args) {
+    if (!args.BandChars.every(v => v.length === 1)) return "INVALID_BANDC";
+    if (!args.InputChars.every(v => v.length === 1 && args.BandChars.includes(v))) return "INVALID_INC";
+    if (!(args.EmptyChar.length === 1 && args.BandChars.includes(args.EmptyChar))) return "INVALID_EMPTC";
+    if (!args.BandValue.split('').every(v => v.length === 0 || args.InputChars.includes(v) || args.EmptyChar === v)) return "INVALID_BANDV";
+    if (!args.Transitions.every(v => v.oChar.length === 1 && args.BandChars.includes(v.oChar) && v.nChar.length === 1 && args.BandChars.includes(v.nChar) && (v.action.toUpperCase() === "R" || v.action.toUpperCase() === "L" || v.action.toUpperCase() === "H"))) return "INVALID_TRANS";
+    return "VALID";
+}
+TuringMachine.prototype.validate = validate;
+
+/**
+ * @this {TuringMachine}
+ * @return {"NOT_READY"|"NO_TRANSITION"|"HALT"|"DONE"}
+ */
+function performStep() {
+    if (this.Band === undefined || this.Position === undefined || this.State === undefined || this.EmptyChar === undefined || this.Transitions === undefined) return "NOT_READY";
+    if (!this.Transitions.has(this.State)) return "NO_TRANSITION";
+    let t = this.Transitions.get(this.State);
+    if (!t.has(this.getCharacterAtPosition(this.Position))) return "NO_TRANSITION";
+    let tc = t.get(this.getCharacterAtPosition(this.Position));
+    this.Band.set(this.Position, tc.char);
+    this.State = tc.state;
+    if (tc.action === "H") return "HALT";
+    if (tc.action === "R") this.Position++;
+    if (tc.action === "L") this.Position--;
+    return "DONE";
+}
+TuringMachine.prototype.performStep = performStep;
+
+/**
+ * @name TuringMachine.getCharacterAtPosition
+ * @param {number} position
+ * @this {TuringMachine}
+ * @return {?string}
+ */
+function getCharacterAtPosition(position) {
+    if (this.Band === undefined || this.Position === undefined || this.EmptyChar === undefined) return undefined;
+    return this.Band.get(position)??this.EmptyChar;
+}
+TuringMachine.prototype.getCharacterAtPosition = getCharacterAtPosition;
+
+/**
+ * @this {TuringMachine}
+ * @return {?string}
+ */
+function getCurrentState() {
+    return this.State;
+}
+TuringMachine.prototype.getCurrentState = getCurrentState;
+
+/**
+ * @this {TuringMachine}
+ * @return {?number}
+ */
+function getCurrentPosition() {
+    return this.Position;
+}
+TuringMachine.prototype.getCurrentPosition = getCurrentPosition;
+
+/**
+ * @this {TuringMachine}
+ * @return {?string}
+ */
+function getBandValues() {
+    if (this.Band === undefined) return undefined;
+    return Array.from(this.Band.entries()).sort(([k1,v1],[k2,v2]) => k1 -k2).map(v => v[1]).join("");
+}
+TuringMachine.prototype.getBandValues = getBandValues;
+
+/**
+ * @this {TuringMachine}
+ * @return {boolean}
+ */
+function isReady() {
+    return !(this.Band === undefined || this.Position === undefined || this.State === undefined || this.EmptyChar === undefined || this.Transitions === undefined);
+}
+TuringMachine.prototype.isReady = isReady;
+
+export { TuringMachine as default };
